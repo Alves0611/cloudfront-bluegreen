e9a833c2ad46dd9dd193253b8b0ddc80fefcfcc0
Alves0611 <g.alves0611@gmail.com>
2025-12-30 00:12:47 -0300

    feat: add CloudFront Origin Access Control module

---
+++ terraform/modules/cloudfront-oac/main.tf
@@ -0,0 +1,8 @@
+resource "aws_cloudfront_origin_access_control" "this" {
+  name                              = var.name
+  description                       = var.description
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
