3974cbcb270b12d3bdbf3a52c563dd4c2c22990c
Alves0611 <g.alves0611@gmail.com>
2025-12-30 00:12:49 -0300

    feat: add CloudFront OAC module variables

---
+++ terraform/modules/cloudfront-oac/variables.tf
@@ -0,0 +1,11 @@
+variable "name" {
+  description = "Name of the Origin Access Control"
+  type        = string
+}
+
+variable "description" {
+  description = "Description of the Origin Access Control"
+  type        = string
+  default     = "Origin Access Control for S3 bucket"
+}
+
