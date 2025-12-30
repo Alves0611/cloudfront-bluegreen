61ae8c22d2e2bf1270597324e6e67677e5ef7215
Alves0611 <g.alves0611@gmail.com>
2025-12-30 00:05:12 -0300

    feat: add S3 bucket module main configuration

---
+++ terraform/modules/s3-bucket/main.tf
@@ -0,0 +1,57 @@
+resource "aws_s3_bucket" "this" {
+  bucket        = var.bucket_name
+  force_destroy = var.force_destroy
+
+  tags = merge(
+    var.tags,
+    {
+      Name = var.bucket_name
+    }
+  )
+}
+
+resource "aws_s3_bucket_versioning" "this" {
+  count  = var.versioning_enabled ? 1 : 0
+  bucket = aws_s3_bucket.this.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = var.encryption_algorithm
+    }
+    bucket_key_enabled = var.bucket_key_enabled
+  }
+}
+
+resource "aws_s3_bucket_ownership_controls" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  rule {
+    object_ownership = var.object_ownership
+  }
+}
+
+resource "aws_s3_bucket_acl" "this" {
+  count      = var.enable_acl ? 1 : 0
+  bucket     = aws_s3_bucket.this.id
+  depends_on = [aws_s3_bucket_ownership_controls.this]
+
+  acl = var.acl_value
+}
+
+resource "aws_s3_bucket_public_access_block" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  block_public_acls       = var.block_public_access
+  block_public_policy     = var.block_public_access
+  ignore_public_acls      = var.block_public_access
+  restrict_public_buckets = var.block_public_access
+}
+
