428f94a444fa6646c337c20475c3492edec01fb1
Alves0611 <g.alves0611@gmail.com>
2025-12-30 00:41:04 -0300

    chore: add Terraform version and backend configuration for dev

---
+++ terraform/environments/dev/versions.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
+  }
+  backend "s3" {
+    bucket         = "tfstate-444065722670"
+    key            = "cloudfront/dev/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "state-locking-444065722670"
+  }
+}
+
